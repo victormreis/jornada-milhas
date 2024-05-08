1ca4a6c006fe8778fa81a7e8c1b99b6744fa3cd1
Victor Martins <vitor_jhoul@hotmail.com>
2024-05-08 18:16:08 -0300

    refactor: refactoring homePage and card-search component to conect with api

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Observable } from 'rxjs';
+import { IPromocao } from './../../core/types/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  promocoes: IPromocao[]  = []
+
+  constructor(
+    private promocaoService: PromocaoService
+  ) {}
+
+  ngOnInit(): void {
+    this.listarPromocoes()
+  }
+
+  listarPromocoes(){
+    this.promocaoService.listarPromocoes().subscribe((response) => {
+      this.promocoes = response
+      console.log(this.promocoes)
+    })
+  }
+
+
+
 
 }
--- src/app/shared/card-search/card-search.component.ts
+++ src/app/shared/card-search/card-search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IPromocao } from 'src/app/core/types/types';
 
 @Component({
   selector: 'app-card-search',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-search.component.scss']
 })
 export class CardSearchComponent {
+
+  @Input() promocao! : IPromocao 
 
 }
